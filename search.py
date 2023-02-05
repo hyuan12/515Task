07f9e9333f2ef4e6b7b677b2327bfd96ba5b26f6
HaiYuan <hyuan12@stevens.edu>
2023-02-04 23:59:22 -0500

    test

--- search.py
+++ search.py
@@ -1,49 +1,55 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  1 18:25:43 2023
-
-@author: whale
-"""
-
 def dfs(graph):
     visited = set()
-    nodes = [num for num in graph.keys() if len(graph[num]) != 0]
-    node = min(nodes)
-    #print(node)
-    helper(visited, graph, node)
-    print(visited)
-    
-def helper(visited, graph, node):
+    res = []
+    flag = 0
+    while min_num(visited, graph) is not None:
+        node = min_num(visited, graph)
+        if flag == 0:
+            flag = 1
+        dfs_helper(visited, graph, res ,node)
+
+    return res
+
+def min_num(visited, graph):
+    if len(visited) == len(graph.keys()):
+        return
+    min_node = min([key for key in graph.keys() if key not in visited])
+    return min_node
+
+def dfs_helper(visited, graph, res, node):
     if node not in visited:
-       
         visited.add(node)
-        if node not in graph.keys():
-            return
-        for neighbour in graph[node]:
-            helper(visited, graph, neighbour)
+        res.append(node)
+        stack = graph[node]
+        stack.reverse()
+        for neighbour in stack:
+            dfs_helper(visited, graph, res, neighbour)
             
-#dfs({0: [1], 1: [0, 1, 2], 2: [0], 3: [0, 1, 2, 4], 4: [3]})
+            
+
+import collections
 
 def bfs(graph):
-    nodes = [num for num in graph.keys() if len(graph[num]) != 0]
-    node = min(nodes)
-    visited = []
-    bfs_helper(visited, graph, node)
-    
+    visited = set()
+    flag = 0
+    while min_num(visited, graph) is not None:
+        node = min_num(visited, graph)
+        if flag == 0:
+            flag = 1
+            visited.add(node)
+        bfs_helper(visited, graph, node)
+
+    print(visited)
+    return [visited]
+
+
 def bfs_helper(visited, graph, node):
-  visited.append(node)
-  queue = []
-  queue.append(node)
+    queue = collections.deque([node])
+    while queue:
+        cur_node = queue.popleft()
 
-  while queue:
-    s = queue.pop(0) 
-
-    if s not in graph.keys():
-        continue
-    for neighbour in graph[s]:
-      if neighbour not in visited:
-        visited.append(neighbour)
-        queue.append(neighbour)
-
-#bfs({1: [2,3], 3:[2, 4], 4: [3]})
+        for neighbor in graph[cur_node]:
+            if neighbor in visited:
+                continue
+            queue.append(neighbor)
+            visited.add(neighbor)
