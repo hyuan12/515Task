aee52657510c6bdc4db062c6f2f8439d690f1fdf
hyuan12 <123429322+hyuan12@users.noreply.github.com>
2023-02-09 17:27:25 -0500

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,12 +1,16 @@
 
 def flatten(item):
-    def helper(item, l):
-        if isinstance(item, list):
-            for s in item:
-                helper(s, l)
+    l = []
+    for i in item:
+        helper(i,l)
+    print(l)
 
-        else:
-            l.append(item)
+def helper(item, l):
+    if isinstance(item, list):
+        for s in item:
+            helper(s, l)
+    else:
+        l.append(item)
 
 
 
