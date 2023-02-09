573e374b4ff353346277b817b35ff21484cf2636
hyuan12 <123429322+hyuan12@users.noreply.github.com>
2023-02-09 18:16:24 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,8 +1,7 @@
-
 def show_triangle(n):
     def triangle(n, i=0, res=[]):
         if i == n:
             return res
         res.append(" " * (n - i - 1) + "* " * (i + 1))
         return triangle(n, i + 1, res)
-    print("\n".join(triangle(n)))
+    return ("\n".join(triangle(n)))
