5724088da9935a2ebc253857a3d9ce06430ffb6e
hyuan12 <123429322+hyuan12@users.noreply.github.com>
2023-02-09 18:28:41 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,17 +1,8 @@
 def show_triangle(n):
-    if n == 0:
-        return
-
-    triangle_rows = triangle(n)
-    for row in triangle_rows:
-        print(row)
-
-def triangle(n):
-    if n == 0:
-        return []
-    if n == 1:
-        return ['*']
-
-    previous_rows = triangle(n - 1)
-    current_row = ' ' * (len(previous_rows) - 1) + '* ' * n
-    return previous_rows + [current_row]
+    def triangle(n, i=0, res=[]):
+        if i == n:
+            return res
+        res.append(" " * (n - i - 1) + "* " * (i + 1))
+        return triangle(n, i + 1, res)
+    print("\n".join(triangle(n)))
+    return "\n".join(triangle(n))
